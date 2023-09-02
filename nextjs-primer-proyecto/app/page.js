07fb62879f9fac07dbe23be7d6b43c1601df8dce
Lucas Speziale <lucasspeziale@MacBook-Pro-de-Lucas.local>
2023-09-02 18:54:52 -0300

    next

--- nextjs-primer-proyecto/app/page.js
+++ nextjs-primer-proyecto/app/page.js
@@ -1,14 +1,17 @@
-
-
- async function getUsers(){
-   const res =  await fetch('https://reqres.in/api/users')
-   const data = await res.json()
+async function getUsers() {
+  const res = await fetch("https://reqres.in/api/users?page=2");
+  const data = await res.json();
+  return data.data
 }
 
-
-
-export default function HomePage(){
-  return(
-    <div className="">HomePage</div>
-  )
-}  
+async function HomePage() {
+  const users = await getUsers();
+  return <div className="bg-black">
+    <h1>{users.first_name}  {users.last_name}</h1>
+    <img src={users.avatar}/>
+    
+  
+    
+  </div>;
+}
+export default HomePage;
