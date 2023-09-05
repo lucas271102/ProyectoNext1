51642fc06b90496782ced9b3850736f9aecfaa07
Lucas Speziale <lucasspeziale@MacBook-Pro-de-Lucas.local>
2023-09-05 16:41:24 -0300

    users

--- nextjs-primer-proyecto/app/page.js
+++ nextjs-primer-proyecto/app/page.js
@@ -1,17 +1,31 @@
-async function getUsers() {
+async function fetchUsers() {
   const res = await fetch("https://reqres.in/api/users?page=2");
   const data = await res.json();
+  console.log(data)
   return data.data
 }
 
 async function HomePage() {
-  const users = await getUsers();
-  return <div className="bg-black">
-    <h1 className="bg-black">{users.first_name}  {users.last_name}</h1>
-    <img src={users.avatar}/>
-    
-  
-    
-  </div>;
+  const users = await fetchUsers();
+  return (
+    <>
+     <h1 className="text-center p-4  text-xl">Lista de usuarios registrados</h1>
+
+      <div className="flex gap-2 flex-wrap items-center justify-center container mx-auto bg-slate-200 rounded-md p-2">
+        {users.map(user => (
+          <div >
+
+
+            <h2 className="text-center text-black">{user.first_name}  {user.last_name}</h2>
+            <img src={user.avatar} className="w-36 h-36  rounded-md" />
+          </div>
+
+        ))}
+
+
+
+      </div>
+    </>
+  )
 }
 export default HomePage;
