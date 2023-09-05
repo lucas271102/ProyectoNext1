0c31f6ba965b37fffae4ff387b517ca218ed828a
Lucas Speziale <lucasspeziale@MacBook-Pro-de-Lucas.local>
2023-09-05 16:53:54 -0300

    flex

--- nextjs-primer-proyecto/app/page.js
+++ nextjs-primer-proyecto/app/page.js
@@ -13,10 +13,11 @@ async function HomePage() {
 
       <div className="flex gap-2 flex-wrap items-center justify-center container mx-auto bg-slate-200 rounded-md p-2">
         {users.map(user => (
-          <div >
+          <div  >
 
 
             <h2 className="text-center text-black">{user.first_name}  {user.last_name}</h2>
+            <h3 className="text-black text-center">Email : {user.email}</h3>
             <img src={user.avatar} className="w-36 h-36  rounded-md" />
           </div>
 
