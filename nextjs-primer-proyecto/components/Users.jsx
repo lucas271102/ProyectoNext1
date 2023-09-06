78f071a5a04b1ec6efd9cf876ce0fc2a43ad088f
lucas271102 <lucas001spe@gmail.com>
2023-09-06 15:53:47 -0300

    next

--- nextjs-primer-proyecto/components/Users.jsx
+++ nextjs-primer-proyecto/components/Users.jsx
@@ -3,6 +3,7 @@
 'use client'
 
 
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -17,9 +18,8 @@ const Users = ({users}) => {
 
       <div className="flex gap-2 flex-wrap items-center justify-center container mx-auto bg-slate-200 rounded-md p-2">
         {users.map((user) => (
-          <div onClick={()=>{
-            alert(user.id, user.email)
-          }} className='border rounded-md border-black bg-white'>
+          <Link key={user.id} href={`/users/${user.id}`}
+           className='border rounded-md p-3 border-black bg-white'>
             <h2 className=" text-black">
               {user.first_name} {user.last_name}
             </h2>
@@ -28,7 +28,7 @@ const Users = ({users}) => {
               src={user.avatar}
               className="w-36 h-36  rounded-full flex justify-center items-center border border-black"
             />
-          </div>
+          </Link>
         ))}
       </div>
     
